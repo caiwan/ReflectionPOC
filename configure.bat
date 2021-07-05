391e367e233a86d5fa55c1c1e3bc0f0e7f496bfc
Caiwan <caiwan.lupus@gmail.com>
2021-07-05 20:00:26 +0200

    WIP, Package

--- configure.bat
+++ configure.bat
@@ -3,6 +3,9 @@
 if not exist msvc md msvc
 if exist msvc\CMakeCache.txt del msvc\CMakeCache.txt
 
+FOR /F "tokens=* USEBACKQ" %%F IN (`git describe --always`) DO ( SET git_describe=%%F )
+echo GIT TOKENS = %git_describe%
+
 cd msvc
-cmake -G "Visual Studio 16 2019" -A x64 ../ 
+cmake -DCMAKE_INSTALL_PREFIX=../deploy/%git_describe% -G "Visual Studio 16 2019" -A x64 ../
 cd ..
